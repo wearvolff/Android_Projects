fab3c097772e10cb666329ba6246a32dd7226f48
Vadim <wearvolff@gmail.com>
2018-05-12 19:59:05 +0300

    add item to list without keyboard

--- app/src/main/java/com/example/user/mylist/MainActivity.java
+++ app/src/main/java/com/example/user/mylist/MainActivity.java
@@ -2,24 +2,24 @@ package com.example.user.mylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private TextView text;
+    private EditText text;
     private ListView listView;
-    private String[] data = new String[1];
-    private  ArrayAdapter adapterList;
+    private List<String> data = new ArrayList<>();
+    private ArrayAdapter adapterList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Button click
         button = findViewById(R.id.button);
-        text = findViewById(R.id.text);
+        text = findViewById(R.id.editText);
         listView = findViewById(R.id.listView);
 
         adapterList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
@@ -37,18 +37,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //if text is empty
-                if (text.getText() == ""){
+                if (text.getText().toString().matches("")){
                     Toast toast = Toast.makeText(getApplicationContext(), "Введите текст", Toast.LENGTH_LONG);
                     toast.show();
                 }
                 //if not empty
-                if (data.length >= 1){
-                    String []newData = new String[data.length+5];
-                    System.arraycopy(data,0, newData, 0, data.length);
-                    data = newData;
-                }
-
+                data.add(text.getText().toString());
+                adapterList.notifyDataSetChanged();
+                //Clear input field
+                text.setText("");
             }
+
         });
     }
 
